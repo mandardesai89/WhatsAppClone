4b588b769f0bdd3df72e425bde829e6ea63ffb18
Mandar Desai <desaimandar2@gmail.com>
2021-01-02 11:55:53 +0530

    First Commit
    This is WhatsApp Clone app. Developed by using Xcode Swift Without using SwiftUI

---
+++ WhatsAppClone/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsAppClone
+//
+//  Created by Technology Software Group on 27/12/20.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
